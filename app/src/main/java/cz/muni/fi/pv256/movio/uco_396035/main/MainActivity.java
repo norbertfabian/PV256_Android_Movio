fa3902cc39cc40da94df82d031e08f703bc5b6bc
fabian <fabian@mathesio.cz>
2017-10-19 23:18:44 +0200

    Rename package + implement Task2

---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco_396035/main/MainActivity.java
@@ -0,0 +1,57 @@
+package cz.muni.fi.pv256.movio.uco_396035.main;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.muni.fi.pv256.movio.uco_396035.R;
+import cz.muni.fi.pv256.movio.uco_396035.data.Movie;
+import cz.muni.fi.pv256.movio.uco_396035.detail.DetailActivity;
+import cz.muni.fi.pv256.movio.uco_396035.detail.DetailFragment;
+
+
+public class MainActivity extends AppCompatActivity implements MainFragment.OnMovieSelectListener {
+
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        if(findViewById(R.id.movie_detail_container) != null) {
+            // The detail container view will be present only in the large-screen layouts
+            // (res/layout-sw600dp). If this view is present, then the activity should be
+            // in two-pane mode.
+            mTwoPane = true;
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            if(savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.movie_detail_container, new DetailFragment(), DetailFragment.TAG)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
+        }
+    }
+
+    @Override
+    public void onMovieSelect(Movie movie) {
+        if(mTwoPane) {
+            FragmentManager fm = getSupportFragmentManager();
+
+            DetailFragment fragment = DetailFragment.newInstance(movie);
+            fm.beginTransaction()
+                    .replace(R.id.movie_detail_container, fragment, DetailFragment.TAG)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_MOVIE, movie);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco_396035/main/MainFragment.java
@@ -0,0 +1,122 @@
+package cz.muni.fi.pv256.movio.uco_396035.main;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import cz.muni.fi.pv256.movio.uco_396035.data.Movie;
+import cz.muni.fi.pv256.movio.uco_396035.R;
+import cz.muni.fi.pv256.movio.uco_396035.data.MovieData;
+
+
+/**
+ * Created by fabian on 19.10.2017.
+ */
+
+public class MainFragment extends Fragment {
+
+    private static final String TAG = MainFragment.class.getSimpleName();
+    private static final String SELECTED_KEY = "selected_position";
+
+    private int mPosition = ListView.INVALID_POSITION;
+    private OnMovieSelectListener mListener;
+    private Context mContext;
+    private ListView mListView;
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+
+        try {
+            mListener = (OnMovieSelectListener) activity;
+        } catch (ClassCastException e) {
+            Log.e(TAG, "Activity must implement OnMovieSelectListener", e);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        // Avoid leaking the Activity
+        mListener = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = getActivity().getApplicationContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        fillListView(view);
+
+        if(savedInstanceState != null && savedInstanceState.containsKey(SELECTED_KEY)) {
+            mPosition = savedInstanceState.getInt(SELECTED_KEY);
+
+            if(mPosition != ListView.INVALID_POSITION) {
+                mListView.smoothScrollToPosition(mPosition);
+            }
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if(mPosition != ListView.INVALID_POSITION) {
+            outState.putInt(SELECTED_KEY, mPosition);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private void fillListView(View rootView) {
+        ArrayList<Movie> movieList = MovieData.getInstance().getMovieList();
+        mListView = rootView.findViewById(R.id.listview_movies);
+
+        if(movieList != null && !movieList.isEmpty()) {
+            setAdapter(mListView, movieList);
+        }
+    }
+
+    private void setAdapter(ListView filmLV, final ArrayList<Movie> movieList) {
+        MovieAdapter adapter = new MovieAdapter(mContext, movieList);
+        filmLV.setAdapter(adapter);
+
+        filmLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mPosition = position;
+                mListener.onMovieSelect(movieList.get(position));
+            }
+        });
+
+        filmLV.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(mContext, movieList.get(position).getTitle(), Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+            }
+        });
+    }
+
+    public interface OnMovieSelectListener {
+        void onMovieSelect(Movie movie);
+    }
+}
